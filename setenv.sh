e9111532ba215e0204001d1b3bcede4f8a4c4952
Wenhao Ji <predator.ray@gmail.com>
2013-07-11 22:07:47 +0800

    Initial Commit

---
+++ setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# set the environment variables for GoAgent here
+#
+
+# GoAgent directory
+# export GOAGENT_HOME=/usr/local/goagent
+
+# stdout & stderr
+# export GOAGENT_OUT=$GOAGENT_HOME/local/logs/goagent.log
